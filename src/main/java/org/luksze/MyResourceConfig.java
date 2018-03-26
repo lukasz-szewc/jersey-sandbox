c983a1d7c3ad7b64301e12e689ced6bf945786a2
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2018-03-26 22:40:37 +0200

    added test for not accepted requests

--- src/main/java/org/luksze/MyResourceConfig.java
+++ src/main/java/org/luksze/MyResourceConfig.java
@@ -7,6 +7,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 class MyResourceConfig extends ResourceConfig {
+
+    public static final String MY_CUSTOM_TEXT = "My custom text";
+
     MyResourceConfig() {
         register(MyResource.class);
         register(new NotAcceptableExceptionExceptionMapper());
@@ -15,7 +18,7 @@ class MyResourceConfig extends ResourceConfig {
     static class NotAcceptableExceptionExceptionMapper implements ExceptionMapper<NotAcceptableException> {
         @Override
         public Response toResponse(NotAcceptableException exception) {
-            return Response.fromResponse(exception.getResponse()).entity("My custom text").build();
+            return Response.fromResponse(exception.getResponse()).entity(MY_CUSTOM_TEXT).build();
         }
     }
 }
--- src/test/java/org/luksze/JettyResourceTest.java
+++ src/test/java/org/luksze/JettyResourceTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_SVG_XML_TYPE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JettyResourceTest {
@@ -31,9 +35,6 @@ class JettyResourceTest {
         server.stop();
     }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
     @Test
     void testGetIt() {
         String responseMsg = target.path("myresource").request().get(String.class);
@@ -44,5 +45,18 @@ class JettyResourceTest {
     void zoneIdGet() {
         String responseMsg = target.path("zoned").request().get(String.class);
         assertEquals("1970-01-01T01:00+01:00[Europe/Warsaw]", responseMsg);
+    }
+
+    @Test
+    void notAcceptedRequest() {
+        //given
+        Invocation invocation = target.path("myresource").request(APPLICATION_SVG_XML_TYPE).buildGet();
+
+        //when
+        Response response = invocation.invoke();
+
+        //then
+        assertEquals(406, response.getStatus());
+        assertEquals("My custom text", MyResourceConfig.MY_CUSTOM_TEXT);
     }
 }
